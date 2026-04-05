8722996866febd30cdc6892fcd57d8a77a113c9a
leschunc <leschunc@student.42lisboa.com>
2026-04-05 11:31:44 +0100

    auto

---
+++ Makefile
@@ -0,0 +1,38 @@
+NAME		= philo
+CC			= cc
+CFLAGS		= -Wall -Werror -Wextra
+VFLAG		= --tool=helgrind 
+
+OBJ_DIR		= obj/
+SRC_DIR		= src/
+
+SRC_FILES	= main.c
+SRCS		= $(addprefix $(SRC_DIR), $(SRC_FILES))
+OBJS		= $(addprefix $(OBJ_DIR), $(SRC_FILES:.c=.o))
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+    
+$(OBJ_DIR)%.o: $(SRC_DIR)%.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@rm -rf $(OBJ_DIR)
+
+fclean: clean
+	@rm -f $(NAME)
+
+re: fclean all
+
+r:
+	$(MAKE)
+	./philo
+
+v:
+	$(MAKE)
+	./valgrind $(VFLAG) ./philo
+
+.PHONY: all clean fclean re
---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+pthread_mutex_t	turn;
+
+void	*steal(void *arg)
+{
+	int	i;
+
+	i = 10;
+	(void)arg;
+	while (i--)
+	{
+		pthread_mutex_lock(&turn);
+		printf("ive stolen\n");
+		pthread_mutex_unlock(&turn);
+		usleep(100);
+	}
+	return (NULL);
+}
+
+void	init(void)
+{
+	pthread_t	thief;
+
+	pthread_mutex_init(&turn, NULL);
+	pthread_create(&thief, NULL, steal, NULL);
+	pthread_join(thief, NULL);
+	pthread_mutex_destroy(&turn);
+}
+
+int	main(int argc, char const *argv[])
+{
+	(void)argc;
+	(void)argv;
+	init();
+	// sleep(1);
+	return (0);
+}
