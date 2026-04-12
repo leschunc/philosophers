0a07b04b28b28663f962cb380115fe09cdf57743
leschunc <leschunc@student.42lisboa.com>
2026-04-12 07:21:10 +0100

    auto

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -14,40 +14,47 @@ void	turn_minds_off(t_context *c)
 	}
 }
 
+bool	safe_inspec(t_context *c, int i)
+{
+	static int	finished = 0;
+	suseconds_t	now;
+
+	now = get_time(c->start);
+	pthread_mutex_lock(c->inspec + i);
+	if (c->set[CYCLE] == c->mind[i].meals)
+	{
+		finished++;
+		if (finished == c->set[NUM])
+		{
+			pthread_mutex_unlock(c->inspec + i);
+			return (false);
+		}
+	}
+	else if (now - c->mind[i].last_meal >= c->set[DIE])
+	{
+		// printf(DIED, get_time(c->mind[i].start), c->mind[i].whoami);
+		pthread_mutex_unlock(c->inspec + i);
+		turn_minds_off(c);
+		return (false);
+	}
+	pthread_mutex_unlock(c->inspec + i);
+	return (true);
+}
+
 void	*fate(void *ref)
 {
 	t_context	*c;
-	suseconds_t	now;
 	int			i;
-	int			finished;
 
-	finished = 0;
 	c = ref;
 	i = 0;
 	while (1)
 	{
 		if (i < c->set[NUM])
 		{
-			now = get_time(c->start);
-			pthread_mutex_lock(c->inspec + i);
-			if (c->set[CYCLE] == c->mind[i].meals)
-			{
-				finished++;
-				if (finished == c->set[NUM])
-				{
-					pthread_mutex_unlock(c->inspec + i);
-					return (NULL);
-				}
-			}
-			else if (now - c->mind[i].last_meal >= c->set[DIE])
-			{
-				printf(DIED, get_time(c->mind[i].start), c->mind[i].whoami);
-				pthread_mutex_unlock(c->inspec + i);
-				turn_minds_off(c);
+			if (safe_inspec(c, i) == false)
 				return (NULL);
-			}
-			pthread_mutex_unlock(c->inspec + i);
-			usleep(100);
+			usleep(10);
 			i++;
 		}
 		else
--- philo/src/philos.c
+++ philo/src/philos.c
@@ -24,13 +24,13 @@ bool	set_last_meal(t_mind *m)
 
 	now = get_time(m->start);
 	pthread_mutex_lock(m->inspec);
-	if (m->last_meal == -1)
-	{
-		pthread_mutex_unlock(m->inspec);
-		// printf("i was killed by monitor\n");
-		return (false);
-		// return (false);
-	}
+	// if (m->last_meal == -1)
+	// {
+	// 	pthread_mutex_unlock(m->inspec);
+	// 	// printf("i was killed by monitor\n");
+	// 	return (false);
+	// 	// return (false);
+	// }
 	m->last_meal = now;
 	m->meals++;
 	if (m->set[CYCLE] != 0 && m->meals == m->set[CYCLE])
@@ -42,31 +42,60 @@ bool	set_last_meal(t_mind *m)
 	return (true);
 }
 
-void	eat_lr(t_mind *m)
+bool	eat_lr(t_mind *m)
 {
+	if (i_am_dead(m) == true)
+		return (false);
 	pthread_mutex_lock(m->l_fork);
+	if (i_am_dead(m) == true)
+	{
+		pthread_mutex_unlock(m->l_fork);
+		return (false);
+	}
 	printf(FORK, get_time(m->start), m->whoami);
 	pthread_mutex_lock(m->r_fork);
+	if (i_am_dead(m) == true)
+	{
+		pthread_mutex_unlock(m->r_fork);
+		pthread_mutex_unlock(m->l_fork);
+		return (false);
+	}
 	printf(FORK, get_time(m->start), m->whoami);
 	pthread_mutex_unlock(m->l_fork);
 	pthread_mutex_unlock(m->r_fork);
+	return (true);
 }
 
-void	eat_rl(t_mind *m)
+bool	eat_rl(t_mind *m)
 {
+	if (i_am_dead(m) == true)
+		return (false);
 	pthread_mutex_lock(m->r_fork);
+	if (i_am_dead(m) == true)
+	{
+		pthread_mutex_unlock(m->r_fork);
+		return (false);
+	}
 	printf(FORK, get_time(m->start), m->whoami);
 	pthread_mutex_lock(m->l_fork);
+	if (i_am_dead(m) == true)
+	{
+		pthread_mutex_unlock(m->l_fork);
+		pthread_mutex_unlock(m->r_fork);
+		return (false);
+	}
 	printf(FORK, get_time(m->start), m->whoami);
 	pthread_mutex_unlock(m->r_fork);
 	pthread_mutex_unlock(m->l_fork);
+	return (true);
 }
 
 bool	grab(t_mind *m)
 {
 	if (m->whoami % 2 == 0)
 	{
-		eat_lr(m);
+		if (eat_lr(m) == false)
+			return (false);
 		printf(EATS, get_time(m->start), m->whoami);
 		if (set_last_meal(m) == false)
 			return (false);
@@ -75,7 +104,8 @@ bool	grab(t_mind *m)
 	else
 	{
 		usleep(1000);
-		eat_rl(m);
+		if (eat_rl(m) == false)
+			return (false);
 		printf(EATS, get_time(m->start), m->whoami);
 		if (set_last_meal(m) == false)
 			return (false);
@@ -103,17 +133,27 @@ void	*daily(void *ref)
 	m = (t_mind *)ref;
 	while (1)
 	{
+		// if (i_am_dead == true)
+		// 	return ((void *)false);
 		printf(THNK, get_time(m->start), m->whoami);
 		if (grab(m) == false)
 			return ((void *)false);
-		printf(THNK, get_time(m->start), m->whoami);
+		if (i_am_dead(m) == true)
+			return ((void *)false);
+		printf(NAPS, get_time(m->start), m->whoami);
 		if (m->whoami % 2)
 		{
 			if (am_i_dead_wait(m, m->set[REST] * 1e3))
+			{
+				// printf(DIED, get_time(m->start), m->whoami);
 				return ((void *)false);
+			}
 		}
 		else if (am_i_dead_wait(m, m->set[REST] * 1e3 + 100))
+		{
+			// printf(DIED, get_time(m->start), m->whoami);
 			return ((void *)false);
+		}
 	}
 	return ((void *)false);
 }
