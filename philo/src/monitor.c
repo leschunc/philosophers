9e6293d82450a4347d772c5d3f99815b4930d4de
leschunc <leschunc@student.42lisboa.com>
2026-04-17 19:21:37 +0100

    auto

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   monitor.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leschunc <leschunc@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: leschunc <leschunc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/13 23:38:18 by leschunc          #+#    #+#             */
-/*   Updated: 2026/04/17 17:03:39 by leschunc         ###   ########.fr       */
+/*   Created: 2026/04/17 19:21:07 by leschunc          #+#    #+#             */
+/*   Updated: 2026/04/17 19:21:26 by leschunc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,12 @@ bool	safe_inspec(t_context *c, int i)
 
 	now = get_time(c->start);
 	lock(c->inspec + i);
-	if (c->mind[i].meals > 0 && c->set[CYC] == c->mind[i].meals)
+	if (c->set[CYC] && c->set[CYC] == c->mind[i].meals)
 	{
 		finished++;
+		c->mind[i].meals = -1;
 		if (finished == c->set[NUM])
-		{
-			unlock(c->inspec + i);
-			return (false);
-		}
+			return (unlock(c->inspec + i), false);
 	}
 	else if (now - c->mind[i].last_meal > c->set[DIE])
 	{
